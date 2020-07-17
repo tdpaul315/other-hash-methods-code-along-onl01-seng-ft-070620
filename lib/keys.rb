d6c81943a299764630f53f0456400b818b5bfddf
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-17 16:57:24 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,6 +1,6 @@
 
 
-This is the groceries hash we'll be passing in to the get_the_keys method
+# This is the groceries hash we'll be passing in to the get_the_keys method
 
 groceries = {
   dairy: ["milk", "yogurt", "cheese"],
